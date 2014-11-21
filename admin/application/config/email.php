dacbef2ad9fd7b6ad06bd6a340eded6ce3ceee5a
wohlig <chintan@wohlig.com>
2014-11-21 10:36:25 +0530

    Email Git

--- admin/application/config/email.php
+++ admin/application/config/email.php
@@ -8,5 +8,5 @@ $config['mailtype'] = 'html';
 $config['smtp_timeout'] = 5;
 $config['smtp_host'] = 'smtp.mandrillapp.com';
 $config['smtp_port'] = 587;
-$config['smtp_user'] = 'noreply@toy-kraft.com';
-$config['smtp_pass'] = 'SfDEXvA0lBII4qx0uRClOA';
+$config['smtp_user'] = 'noreplay@toy-kraft.com';
+$config['smtp_pass'] = 'yM8ie1lgVMaYob_aXkIwXA';
